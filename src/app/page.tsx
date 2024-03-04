ae397616d2c1af8106f9e9e59d3d10b216ef46fa
Krish-Parekh <krishparekh303@gmail.com>
2024-03-04 12:38:49 +0530

    adding description for each algorithms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import { Select } from "@/components/Input/Select";
 import { Slider } from "@/components/Input/Slider";
 import { useSortingAlgorithmContext } from "@/context/Vizualizer";
 import { SortingAlgorithmType } from "@/lib/types";
-import { algorithmOptions, generateAnimationArray } from "@/lib/utils";
+import {
+  algorithmOptions,
+  generateAnimationArray,
+  sortingAlgorithmsData,
+} from "@/lib/utils";
 import { FaPlayCircle } from "react-icons/fa";
 import { RxReset } from "react-icons/rx";
 
@@ -70,6 +74,42 @@ export default function Home() {
                   <FaPlayCircle className="text-system-green60 h-8 w-8" />
                 )}
               </button>
+            </div>
+            <div className="hidden sm:flex absolute top-[120%] left-0 w-full">
+              <div className="flex w-full text-gray-400 p-4 rounded border border-system-purple20 bg-system-purple80 bg-opacity-10 gap-6">
+                <div className="flex flex-col items-start justify-start w-3/4">
+                  <h3 className="text-lg">
+                    {sortingAlgorithmsData[selectedAlgorithm].title}
+                  </h3>
+                  <p className="text-sm text-grey-500 pt-2">
+                    {sortingAlgorithmsData[selectedAlgorithm].description}
+                  </p>
+                </div>
+
+                <div className="flex flex-col items-start justify-start w-1/4 gap-2">
+                  <h3 className="text-lg">Time Complexity</h3>
+                  <div className="flex flex-col gap-2">
+                    <p className="flex w-full text-sm text-gray-500">
+                      <span className="w-28">Worst Case:</span>
+                      <span>
+                        {sortingAlgorithmsData[selectedAlgorithm].worstCase}
+                      </span>
+                    </p>
+                    <p className="flex w-full text-sm text-gray-500">
+                      <span className="w-28">Average Case:</span>
+                      <span>
+                        {sortingAlgorithmsData[selectedAlgorithm].averageCase}
+                      </span>
+                    </p>
+                    <p className="flex w-full text-sm text-gray-500">
+                      <span className="w-28">Best Case:</span>
+                      <span>
+                        {sortingAlgorithmsData[selectedAlgorithm].bestCase}
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -22,7 +22,7 @@ export const algorithmOptions = [
   { label: "Merge", value: "merge" },
   { label: "Insertion", value: "insertion" },
   { label: "Selection", value: "selection" },
-  { label: "Shell",  value: "shell" }
+  { label: "Shell", value: "shell" },
 ];
 
 export function generateAnimationArray(
@@ -35,17 +35,76 @@ export function generateAnimationArray(
   switch (selectedAlgorithm) {
     case "bubble":
       return generateBubbleSortAnimationArray(isSorting, array, runAnimation);
-    case "quick": 
+    case "quick":
       return generateQuickSortAnimationArray(isSorting, array, runAnimation);
     case "insertion":
-      return generateInsertionSortAnimationArray(isSorting, array, runAnimation);
+      return generateInsertionSortAnimationArray(
+        isSorting,
+        array,
+        runAnimation
+      );
     case "merge":
       return generateMergeSortAnimationArray(isSorting, array, runAnimation);
-    case "selection": 
-      return generateSelectionSortAnimationArray(isSorting, array, runAnimation);
-    case "shell": 
-    return generateShellSortAnimationArray(isSorting, array, runAnimation);
+    case "selection":
+      return generateSelectionSortAnimationArray(
+        isSorting,
+        array,
+        runAnimation
+      );
+    case "shell":
+      return generateShellSortAnimationArray(isSorting, array, runAnimation);
     default:
       return;
   }
 }
+
+export const sortingAlgorithmsData = {
+  bubble: {
+    title: "Bubble Sort",
+    description:
+      "A simple comparison-based sorting algorithm. Bubble sort repeatedly compares and swaps adjacent elements if they are in the wrong order, moving larger elements towards the end with each pass through the list. This process continues until the list is sorted and no more swaps are needed.",
+    worstCase: "O(n²)",
+    averageCase: "O(n²)",
+    bestCase: "O(n)",
+  },
+  insertion: {
+    title: "Insertion Sort",
+    description:
+      "Insertion sort builds the final sorted array one element at a time, by repeatedly taking the next unsorted element and inserting it into its correct position among the previously sorted elements. This process continues until all elements have been inserted into their proper place, resulting in a sorted list.",
+    worstCase: "O(n²)",
+    averageCase: "O(n²)",
+    bestCase: "O(n)",
+  },
+  selection: {
+    title: "Selection Sort",
+    description:
+      "Selection sort works by repeatedly finding the minimum element from the unsorted portion of the list and swapping it with the element at the current position. This process is continued for each position in the list, moving the boundary of the sorted and unsorted portions one element forward each time until the entire list is sorted.",
+    worstCase: "O(n²)",
+    averageCase: "O(n²)",
+    bestCase: "O(n²)",
+  },
+  merge: {
+    title: "Merge Sort",
+    description:
+      "Merge sort divides the unsorted list into n sublists, each containing one element (a list of one element is considered sorted), and then repeatedly merges these sublists to produce new sorted sublists until there is only one sublist remaining, which is the sorted list. This algorithm uses a divide-and-conquer approach, splitting the list in half recursively and merging the sorted halves back together.",
+    worstCase: "O(n log n)",
+    averageCase: "O(n log n)",
+    bestCase: "O(n log n)",
+  },
+  quick: {
+    title: "Quick Sort",
+    description:
+      "Quick sort selects a 'pivot' element from the array and partitions the other elements into two sub-arrays, according to whether they are less than or greater than the pivot. The sub-arrays are then sorted recursively, and the sorted sub-arrays are combined with the pivot to form the sorted array.",
+    worstCase: "O(n²)",
+    averageCase: "O(n log n)",
+    bestCase: "O(n log n)",
+  },
+  shell: {
+    title: "Shell Sort",
+    description:
+      "Shell sort is a generalization of insertion sort that allows the exchange of items that are far apart. The idea is to arrange the list of elements so that, starting anywhere, considering every hth element gives a sorted list. Such a list is said to be h-sorted. By h-sorting for some large values of h, we can move elements in the array long distances towards their final position. Shell sort is a sequence of interleaved insertion sorts, the interval of which is gradually reduced to 1, which is the same as an insertion sort.",
+    worstCase: "O(n²)",
+    averageCase: "O(n log n)",
+    bestCase: "O(n log n)",
+  }
+};
